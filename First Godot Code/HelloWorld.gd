8ccea5a91f398a94d34bc6e0e62b6331f1ae4b9c
Ocram <ocram_redqueen@outlook.com>
2019-12-12 22:46:11 -0300

    Test commit ndeah Agregado de archivo Txt.txt para probar commit.

--- First Godot Code/HelloWorld.gd
+++ First Godot Code/HelloWorld.gd
@@ -3,10 +3,10 @@ extends Panel
 var originalText = "Cuando menos te lo esperas...";
 var newText = "No hay fuerza de alma para continuar";
 
+
 func _ready():
 	get_node("Container/Button").connect("pressed", self, "on_button_pressed");
 	get_node("Container/Label").text = originalText;
-
 
 func on_button_pressed():
 		get_node("Container/Label").text = newText;
