bc94729b7622d5708b1036088400030a01f882ef
kevinlink123 <kevin_link_23@hotmail.com>
2019-12-20 16:53:14 -0300

    Features: Pruebas de spawn aleatorio de enemigos. Animacion de sprites. Agregado de colisiones. Primeras bases para armado de GUI's

---
+++ First Project/HUD.gd
@@ -0,0 +1,29 @@
+extends CanvasLayer
+
+signal start_game;
+
+func show_message(text):
+    $MessageLabel.text = text
+    $MessageLabel.show()
+    $MessageTimer.start()
+
+func show_game_over():
+    show_message("Game Over");
+    yield($MessageTimer, "timeout");
+    $MessageLabel.text = "mori3";
+    $MessageLabel.show();
+    yield(get_tree().create_timer(1), 'timeout');
+    $MessageLabel.text = "Ndeah";
+    $MessageLabel.show();
+    $StartButton.show();
+
+func update_score(score):
+    $ScoreLabel.text = str(score)
+
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide();
+
+
+func _on_StartButton_pressed():
+	$StartButton.hide();
+	emit_signal("start_game");
---
+++ First Project/Scenes/Player.gd
@@ -0,0 +1,57 @@
+extends Area2D
+
+signal hit
+signal pene
+
+export var speed = 400;
+var screen_size;
+var velocity = Vector2();
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size;
+	hide();
+	
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		velocity.y += -4;
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 4;
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 4;
+	if Input.is_action_pressed("ui_left"):
+		velocity.x += -4;
+	
+	if Input.is_action_pressed("ui_right") && Input.is_action_pressed("ui_left"):
+		velocity.x = 0;
+	if Input.is_action_pressed("ui_up") && Input.is_action_pressed("ui_down"):
+		velocity.y = 0;
+	
+	if velocity.length() > 0:
+		#$AnimatedSprite.play();
+		get_node("AnimatedSprite").play();
+	else:
+		$AnimatedSprite.stop();
+	position += velocity * delta;
+	position.x = clamp(position.x, 0, screen_size.x);
+	position.y = clamp(position.y, 0, screen_size.y);
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "Right";
+		$AnimatedSprite.flip_v = false;
+		$AnimatedSprite.flip_h = velocity.x < 0;
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "Up";
+		$AnimatedSprite.flip_v = velocity.y > 0;
+		
+	
+	
+func _on_Player_body_entered(body):
+	hide();
+	emit_signal("hit");
+	$CollisionShape2D.set_deferred("disabled", true);
+
+func start(pos):
+	position = pos;
+	show();
+	$CollisionShape2D.disabled = false;
