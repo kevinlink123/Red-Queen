602c56cdae9b14cdbb7595266b86d33d3ce60700
Kevin Ybarra <kevin_link_23@hotmail.com>
2020-03-01 17:42:01 -0300

    refactor(tileset-demo): changed movement script for player node

--- TileSet Editor Demo/Scenes/Player.gd
+++ TileSet Editor Demo/Scenes/Player.gd
@@ -10,16 +10,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass;
+
+
+func _physics_process(delta):
+	var up = Input.is_action_pressed("ui_up");
+	var down = Input.is_action_pressed("ui_down");
+	var left = Input.is_action_pressed("ui_left");
+	var right = Input.is_action_pressed("ui_right");
 	var velocity = Vector2();
-	if Input.is_action_pressed("ui_up"):
+	
+	if up:
 		velocity.y += -1;
-	if Input.is_action_pressed("ui_down"):
+	if down:
 		velocity.y += 1;
-	if Input.is_action_pressed("ui_left"):
+	if left:
 		velocity.x += -1;
-	if Input.is_action_pressed("ui_right"):
+	if right:
 		velocity.x += 1;
 	
-	
-	$".".position.y += velocity.y * speed;
-	$".".position.x += velocity.x * speed;
+	move_and_collide(velocity * speed);
