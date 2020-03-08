93cb7a7ced6561516767540ab5615f3284133608
Kevin Ybarra <kevin.ybarra@gm2dev.com>
2020-03-08 16:12:05 -0300

    feat(tileset-demo): added changes to animation and tilset in general

--- TileSet Editor Demo/Scenes/Player/Player.gd
+++ TileSet Editor Demo/Scenes/Player/Player.gd
@@ -14,19 +14,44 @@ func _process(delta):
 
 
 func _physics_process(delta):
+	
+	
 	var up = Input.is_action_pressed("ui_up");
 	var down = Input.is_action_pressed("ui_down");
 	var left = Input.is_action_pressed("ui_left");
 	var right = Input.is_action_pressed("ui_right");
+	var up_right = up && right;
+	var up_left = up && left;
+	var down_right = down && right;
+	var down_left = down && left;
 	var velocity = Vector2();
 	
 	if up:
+		$AnimatedSprite.play("Back");
 		velocity.y += -1;
+		
 	if down:
+		$AnimatedSprite.play("Front");
 		velocity.y += 1;
+		
 	if left:
+		$AnimatedSprite.play("Left");
 		velocity.x += -1;
+		
 	if right:
+		$AnimatedSprite.play("Right");
 		velocity.x += 1;
+	
+	if up_right || down_right:
+		$AnimatedSprite.play("Right");
+	
+	if up_left || down_left:
+		$AnimatedSprite.play("Left");
+	
+	
+	if !(velocity.x) && !(velocity.y):
+		print("idle");
+		$AnimatedSprite.stop();
+		$AnimatedSprite.set_frame(0);
 	
 	move_and_collide(velocity * speed);
