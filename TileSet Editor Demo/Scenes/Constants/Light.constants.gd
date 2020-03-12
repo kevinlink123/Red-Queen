4861376cd532a91d860d5a178f1853022fb51c57
Kevin Ybarra <kevin.ybarra@gm2dev.com>
2020-03-12 16:39:58 -0300

    feat(tilset-editor-demo): add first city's mockup

---
+++ TileSet Editor Demo/Scenes/Constants/Light.constants.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var lightConstants = {
+	"rotationDegree": {
+		"up": 0,
+		"right": 90,
+		"down": 180,
+		"left": -90
+	}
+}
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- TileSet Editor Demo/Scenes/Player/Player.gd
+++ TileSet Editor Demo/Scenes/Player/Player.gd
@@ -1,9 +1,18 @@
 extends KinematicBody2D
-
+<<<<<<< HEAD
+var lastMovement: String;
 
 export var speed = 6;
 
+onready var lightConstants = $"/root/light_constants".lightConstants;
+
 # Called when the node enters the scene tree for the first time.
+=======
+
+export var speed = 4;
+var moveAble = true;
+
+>>>>>>> dc105ffd374506c14db9569e2b5dfb862e9c5845
 func _ready():
 	pass;
 
@@ -14,44 +23,100 @@ func _process(delta):
 
 
 func _physics_process(delta):
+<<<<<<< HEAD
 	
 	
 	var up = Input.is_action_pressed("ui_up");
 	var down = Input.is_action_pressed("ui_down");
 	var left = Input.is_action_pressed("ui_left");
 	var right = Input.is_action_pressed("ui_right");
-	var up_right = up && right;
-	var up_left = up && left;
-	var down_right = down && right;
-	var down_left = down && left;
 	var velocity = Vector2();
 	
 	if up:
-		$AnimatedSprite.play("Back");
+		if(left):
+			$AnimatedSprite.play("Left");
+		else: if(right):
+			$AnimatedSprite.play("Right");
+		else:
+			$AnimatedSprite.play("Back");
 		velocity.y += -1;
+		$DiwiLight.set_rotation_degrees(lightConstants.rotationDegree.up)
+		lastMovement = "Back";
 		
 	if down:
-		$AnimatedSprite.play("Front");
+		if(left):
+			$AnimatedSprite.play("Left");
+		else: if(right):
+			$AnimatedSprite.play("Right");
+		else:
+			$AnimatedSprite.play("Front");
 		velocity.y += 1;
+		$DiwiLight.set_rotation_degrees(lightConstants.rotationDegree.down)
+		lastMovement = "Front";
 		
 	if left:
 		$AnimatedSprite.play("Left");
 		velocity.x += -1;
+		$DiwiLight.set_rotation_degrees(lightConstants.rotationDegree.left)
+		lastMovement = "Left";
 		
 	if right:
 		$AnimatedSprite.play("Right");
 		velocity.x += 1;
+		$DiwiLight.set_rotation_degrees(lightConstants.rotationDegree.right)
+		lastMovement = "Right";
 	
-	if up_right || down_right:
-		$AnimatedSprite.play("Right");
-	
-	if up_left || down_left:
-		$AnimatedSprite.play("Left");
 	
 	
 	if !(velocity.x) && !(velocity.y):
-		print("idle");
-		$AnimatedSprite.stop();
+		$AnimatedSprite.play("Still " + lastMovement);
 		$AnimatedSprite.set_frame(0);
+=======
+	if moveAble:
+		var up = Input.is_action_pressed("ui_up");
+		var down = Input.is_action_pressed("ui_down");
+		var left = Input.is_action_pressed("ui_left");
+		var right = Input.is_action_pressed("ui_right");
+		if Input.is_action_pressed("ui_shift"):
+			speed = 6
+		else:
+			speed = 2
+		
+		var velocity = Vector2();
+		
+		if up:
+			if (right || left) == false:
+				$AnimatedSprite.play("Back");
+			velocity.y += -1;
+			
+		if down:
+			if (right || left) == false:
+				$AnimatedSprite.play("Front");
+			velocity.y += 1;
+			
+		if left:
+			$AnimatedSprite.play("Left");
+			velocity.x += -1;
+			
+		if right:
+			$AnimatedSprite.play("Right");
+			velocity.x += 1;
+		
+		if !(velocity.x) && !(velocity.y):
+			print("idle");
+			$AnimatedSprite.stop();
+			$AnimatedSprite.set_frame(4);
+		
+		move_and_collide(velocity * speed);
+	else:
+		move_and_collide(Vector2(1,0))
+
+
+func _on_ShallNotPass_body_entered(body):
+	moveAble = false
+	$AnimatedSprite.play("Right")
+>>>>>>> dc105ffd374506c14db9569e2b5dfb862e9c5845
 	
-	move_and_collide(velocity * speed);
+
+func _on_ShallPass_body_entered(body):
+	moveAble = true
